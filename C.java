b8e0913d1c9f7adecb7bd7619cca51aa68776ffd
helena muniz nogueira <helena.nogueira@estudante.ufla.br>
2019-09-11 22:48:49 -0300

    atualizacao 2

---
+++ C.java
@@ -0,0 +1,25 @@
+
+public class C {
+	int C1;
+	float C2;
+	public int getC1() {
+		return C1;
+	}
+	public void setB1(int c1) {
+		C1 = c1;
+	}
+	public float getC2() {
+		return C2;
+	}
+	public void setC2(float c2) {
+		C2 = c2;
+	}
+	public void MC1()
+	{
+		System.out.println("MC1");
+	}
+	public void MC2()
+	{
+		System.out.println("MC2");
+	}
+}
